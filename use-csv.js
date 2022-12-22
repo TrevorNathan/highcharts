1a8639484f8e85b4958ea3203d255105637a9476
TrevorNathan <trevoratwijukire@gmail.com>
2022-12-22 10:28:19 +0300

    ADD: use CSV file

---
+++ use-csv.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const options = {
+		chart: {
+			type: "column",
+			zoomType: "zx",
+		},
+		title: {
+			text: "My Fruits Chart",
+		},
+		yAxis: {
+			title: {
+				text: "Fruits Eaten",
+			},
+		},
+	};
+
+	// Get data from csv file
+	// use JQuery
+	/*
+	$.get("test.csv", (csv) => {
+		options.data = {
+			csv,
+		};
+		Highcharts.chart("container", options);
+	});
+  */
+
+	// use Fetch
+	/*
+	fetch("test.csv")
+		.then((res) => {
+			return res.text();
+		})
+		.then((csv) => {
+			options.data = {
+				csv,
+			};
+			Highcharts.chart("container", options);
+		});
+    */
+
+	// OR
+	options.data = {
+		csvURL: "http://localhost:5500/test.csv",
+		enablePolling: true,
+		dataRefreshRate: 2,
+	};
+	Highcharts.chart("container", options);
+});
