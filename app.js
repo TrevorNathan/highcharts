868f784ac96ff129f4a72cc7261db54e32586725
TrevorNathan <trevoratwijukire@gmail.com>
2022-12-22 09:09:42 +0300

    ADD: Credits

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
 	Highcharts.chart("container", {
 		chart: {
 			type: "column",
-			zoomType: "x",
+		},
+		credits: {
+			//enabled: false,
+			text: "My custom credit",
+			href: "https://google.com",
+			position: {
+				align: "left",
+				x: 10,
+			},
+			style: {
+				fontSize: "15px",
+				color: "red",
+			},
 		},
 		title: {
 			text: "My Fruits Chart",
